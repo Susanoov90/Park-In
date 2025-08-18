1195ff2a48ad0e4d054de9df896800cd1f70188d
KOUAKOU Georgio <georgino.kouak@gmail.com>
2025-08-18 18:13:59 -0400

    Fix port for live reload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.kgv.parkin',
   appName: "Park'In",
-  webDir: 'dist'
+  webDir: 'dist',
+  android: {
+    minWebViewVersion: 55,
+    backgroundColor: "#00000000",
+  },
+  server: {
+    url: "http://localhost:5173", 
+    cleartext: true
+  }
+
 };
+
 
 export default config;
